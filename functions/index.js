a82c163bcbfd433222e3421e26b3937318aaa61f
Serge Martynov <serge.martynov@gmail.com>
2018-10-11 23:54:14 +0300

    Update device token

--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,22 @@
-// The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
+'use strict';
+
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-
-exports.digSand = functions.https.onRequest((request, response) => {
-    response.send("Sandbox is replying!");
-});
+const settings = {timestampsInSnapshots: true};                                                                         // to avoid warning in the console log
+admin.firestore().settings(settings);
 
 
 /**
- * Implemented as a HTTPS callable function: f(data, context)
+ * Sandbox. Implemented as a HTTPS callable function: f(data, context)
  */
-exports.sendLocation = functions.https.onCall((data, context) => {
-    // const location = data.location;                                                                                     // a location passed from the client
-    // const uid = context.auth.uid;                                                                                       // auto-added to the request
-    // const name = context.auth.token.name || null;
-    // const picture = context.auth.token.picture || null;
-    // const email = context.auth.token.email || null;
+exports.digSand = functions.https.onCall((data, context) => {
+    // const location = data.location;                                                                                  // a location passed from the client
+    const uid = context.auth.uid;                                                                                       // context is auto-added to the request
+    const name = context.auth.token.name || null;
+    const picture = context.auth.token.picture || null;
+    const email = context.auth.token.email || null;
 
     // if (location.length === 0) {
     //     throw new functions.https.HttpsError('invalid-argument', 'Empty location provided.');
@@ -24,8 +25,49 @@ exports.sendLocation = functions.https.onCall((data, context) => {
     console.log(data);
 
     return {                                                                                                            // sync return
-        returnCode: "0000",
-        returnMessage: "Location sent",
+        returnCode: "00",
     }
 
+});
+
+
+/**
+ * Send a location from a minor app to major apps
+ * Implemented as a HTTPS callable function: f(data, context)
+ */
+exports.sendLocation = functions.https.onCall((data, context) => {
+    console.log(data);
+
+    return {                                                                                                            // sync return
+        returnCode: "00",
+    }
+
+});
+
+
+/**
+ * Update a stored device token used for FCM
+ * Implemented as a HTTPS callable function: f(data, context)
+ */
+exports.updateDeviceToken = functions.https.onCall((data, context) => {
+    const userUid = context.auth.uid;
+    const deviceToken = data.deviceToken;
+
+    return admin.firestore()
+        .collection('users')
+        .doc(userUid)
+        .set({ deviceToken: deviceToken})
+        .then( (writeResult) => {
+            return {
+                returnCode: "00",
+            }
+        })
+        .catch((error) => {
+            console.log(`User UID ${userUid} error while updating ${deviceToken}: ${error}`);
+            return {
+                returnCode: "01",
+                returnMessage: error,
+            }
+        })
+    ;
 });
