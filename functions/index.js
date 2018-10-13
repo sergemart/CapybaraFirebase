31fc425c91647ec229a0425bfa1631751a90b87c
Serge Martynov <serge.martynov@gmail.com>
2018-10-13 11:48:12 +0300

    Add basic create family data function

--- functions/index.js
+++ functions/index.js
@@ -9,34 +9,13 @@ admin.firestore().settings(settings);
 
 
 /**
- * Sandbox. Implemented as a HTTPS callable function: f(data, context)
- */
-exports.digSand = functions.https.onCall((data, context) => {
-    // const location = data.location;                                                                                  // a location passed from the client
-    const uid = context.auth.uid;                                                                                       // context is auto-added to the request
-    const name = context.auth.token.name || null;
-    const picture = context.auth.token.picture || null;
-    const email = context.auth.token.email || null;
-
-    // if (location.length === 0) {
-    //     throw new functions.https.HttpsError('invalid-argument', 'Empty location provided.');
-    // }
-
-    console.log(data);
-
-    return {                                                                                                            // sync return
-        returnCode: "00",
-    }
-
-});
-
-
-/**
  * Send a location from a minor app to major apps
  * Implemented as a HTTPS callable function: f(data, context)
  */
 exports.sendLocation = functions.https.onCall((data, context) => {
     console.log(data);
+
+    if (!context.auth) throw new functions.https.HttpsError('failed-precondition', "Not authenticated");
 
     return {                                                                                                            // sync return
         returnCode: "00",
@@ -50,10 +29,15 @@ exports.sendLocation = functions.https.onCall((data, context) => {
  * Implemented as a HTTPS callable function: f(data, context)
  */
 exports.updateDeviceToken = functions.https.onCall((data, context) => {
+    if (!context.auth) throw new functions.https.HttpsError('failed-precondition', "Not authenticated");
+
     const userUid = context.auth.uid;
+    // const name = context.auth.token.name || null;
+    // const picture = context.auth.token.picture || null;
+    const email = context.auth.token.email || null;
     const deviceToken = data.deviceToken;
 
-    return admin.firestore()
+    return admin.firestore()                                                                                            // the Firestore client
         .collection('users')
         .doc(userUid)
         .set({ deviceToken: deviceToken})
@@ -63,11 +47,39 @@ exports.updateDeviceToken = functions.https.onCall((data, context) => {
             }
         })
         .catch((error) => {
-            console.log(`User UID ${userUid} error while updating ${deviceToken}: ${error}`);
+            console.log(`User ${email} error while updating ${deviceToken}: ${error}`);
+            throw new functions.https.HttpsError('unknown', error);
+        })
+    ;
+});
+
+
+/**
+ * Create a family
+ * Implemented as a HTTPS callable function: f(data, context)
+ */
+exports.createFamily = functions.https.onCall((data, context) => {
+    if (!context.auth) throw new functions.https.HttpsError('failed-precondition', "Not authenticated");
+
+    const userUid = context.auth.uid;
+    // const name = context.auth.token.name || null;
+    // const picture = context.auth.token.picture || null;
+    const email = context.auth.token.email || null;
+    let familyUid;
+
+    return admin.firestore()                                                                                            // the Firestore client
+        .collection('families')
+        .add({ creator: userUid})
+        .then( (writeResult) => {
+            familyUid = writeResult.id;
             return {
-                returnCode: "01",
-                returnMessage: error,
+                returnCode: "00",
+                familyUid: familyUid,
             }
+        })
+        .catch((error) => {
+            console.log(`User ${email} error while creating ${deviceToken}: ${error}`);
+            throw new functions.https.HttpsError('unknown', error);
         })
     ;
 });
